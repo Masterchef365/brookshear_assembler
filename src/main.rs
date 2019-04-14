8a0e3b419285b71510bb5d5b8466263d7747c289
Masterchef365 <duncan.freeman1@gmail.com>
2019-04-13 23:08:50 -0700

    Parser executes, but only from memory.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::collections::hash_map::HashMap;
+use std::io::prelude::*;
+use std::fs::File;
+
 #[allow(dead_code)]
 mod op_codes {
     pub const LMEM: u8 = 0x01;
@@ -93,6 +96,8 @@ fn translate(program: &[Token]) -> Vec<u8> {
             TokenValue::LabelRef(label) => {
                 if let Some(value) = label_hashmap.get(&label) {
                     output.push(*value)
+                } else {
+                    panic!("Label not found: {}", label);
                 }
             }
             _ => panic!(),
@@ -101,29 +106,118 @@ fn translate(program: &[Token]) -> Vec<u8> {
     return output;
 }
 
-fn data_segment_string(text: String, label: String) -> Vec<Token> {
+fn data_segment(data: &[u8], label: String, end_label: Option<String>) -> Vec<Token> {
     let mut output = Vec::new();
-    let text_ascii = text.into_bytes();
+
+    // Label the first token
     output.push(Token {
         label: Some(label),
-        value: TokenValue::Constant(text_ascii[0]),
+        value: TokenValue::Constant(data[0]),
     });
 
-    for character in text_ascii[1:] {
+    // Add the remaining data
+    for character in data.iter().skip(1).take(data.len() - 2) {
         output.push(Token {
             label: None,
-            value: TokenValue::Constant(character),
+            value: TokenValue::Constant(*character),
         });
     }
 
+    if let Some(end_label) = end_label {
+        output.push(Token {
+            label: Some(end_label),
+            value: TokenValue::Constant(*data.last().unwrap()),
+        });
+    } else {
+        output.push(Token {
+            label: None,
+            value: TokenValue::Constant(*data.last().unwrap()),
+        });
+    }
+
+    // Preserve alignment
+    if output.len() % 2 != 0 {
+        output.push(Token {
+            label: None,
+            value: TokenValue::Constant(0x00),
+        })
+    }
     return output;
 }
 
+fn const_instruction(a: u8, b: u8, c: u8, d: u8, label: Option<String>) -> [Token; 2] {
+    [
+        Token {
+            value: TokenValue::Constant(concat_nibs(a, b)),
+            label,
+        },
+        Token {
+            value: TokenValue::Constant(concat_nibs(c, d)),
+            label: None,
+        },
+    ]
+}
+
+fn set_register(register: u8, value: u8, label: Option<String>) -> [Token; 2] {
+    [
+        Token {
+            value: TokenValue::Constant(concat_nibs(LCON, register)),
+            label,
+        },
+        Token {
+            value: TokenValue::Constant(value),
+            label: None,
+        },
+    ]
+}
+
+fn label_as_arg(
+    opcode: u8,
+    condition_reg: u8,
+    label: String,
+    rejump: Option<String>,
+) -> [Token; 2] {
+    [
+        Token {
+            value: TokenValue::Constant(concat_nibs(opcode, condition_reg)),
+            label: rejump,
+        },
+        Token {
+            value: TokenValue::LabelRef(label),
+            label: None,
+        },
+    ]
+}
+
 fn main() {
-    let main = Vec::new();
+    let mut main: Vec<Token> = Vec::new();
+    main.extend_from_slice(&set_register(1, 0x01, None));
+    main.extend_from_slice(&label_as_arg(LMEM, 0, "string_end".to_string(), None));
+    main.extend_from_slice(&label_as_arg(LMEM, 3, "print_pointer".to_string(), None));
+    main.extend_from_slice(&[
+        Token {
+            value: TokenValue::Constant(concat_nibs(LMEM, 2)),
+            label: Some("print_start".to_string()),
+        },
+        Token {
+            value: TokenValue::LabelRef("string_start".to_string()),
+            label: Some("print_pointer".to_string()),
+        },
+    ]);
+    main.extend_from_slice(&const_instruction(CHAR, 2, 0, 0, None));
+    main.extend_from_slice(&const_instruction(ADD, 3, 3, 1, None));
+    main.extend_from_slice(&label_as_arg(STOR, 3, "print_pointer".to_string(), None));
+    main.extend_from_slice(&label_as_arg(JUMP, 3, "halt".to_string(), None));
+    main.extend_from_slice(&label_as_arg(JUMP, 0, "print_start".to_string(), None));
+    main.extend(data_segment(b"Hello, world!", "string_start".to_string(), Some("string_end".to_string())));
+    main.extend_from_slice(&const_instruction(HALT, 0, 0, 0, Some("halt".to_string())));
 
     let translated_program = translate(&mut main);
     for (position, chunk) in translated_program.chunks(2).enumerate() {
         println!("{:02X}: {:02X}{:02X} ", position * 2, chunk[0], chunk[1]);
     }
+
+    let mut file = File::create("Bitchass.sma").unwrap();
+    file.write_all(&translated_program).unwrap();
+    file.flush().unwrap();
 }
